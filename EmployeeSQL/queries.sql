14e7818eb1336312fc00168f4a0690b87101060a
Meagan Murray-Bruce <meaganmb_11@Meagans-MBP.fios-router.home>
2021-08-16 15:55:02 -0400

    queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -30,4 +30,55 @@ A.emp_no = E.emp_no;
 
 -- List the department of each employee with the following information: employee number, last name,
 -- first name, and department name.
+select A.emp_no,
+E.last_name,
+E.first_name,
+D.dept_name
+from departments as D
+inner join dept_emp as A ON
+D.dept_no = A.dept_no
+inner join employees as E ON
+A.emp_no = E.emp_no;
 
+-- List first name, last name, and sex for employees whose first name is "Hercules"
+-- and last names begin with "B."
+select first_name,
+last_name,
+sex
+from employees
+where first_name = 'Hercules'
+	and last_name like 'B%';
+	
+-- List all employees in the Sales department, including their employee number, last name,
+-- first name, and department name.
+select A.emp_no,
+E.last_name,
+E.first_name,
+D.dept_name
+from departments as D
+inner join dept_emp as A ON
+D.dept_no = A.dept_no
+inner join employees as E ON
+A.emp_no = E.emp_no
+where D.dept_name = 'Sales';
+
+-- List all employees in the Sales and Development departments, including their employee number,
+-- last name, first name, and department name
+select A.emp_no,
+E.last_name,
+E.first_name,
+D.dept_name
+from departments as D
+inner join dept_emp as A ON
+D.dept_no = A.dept_no
+inner join employees as E ON
+A.emp_no = E.emp_no
+where D.dept_name = 'Sales'
+	or D.dept_name = 'Development';
+
+-- In descending order, list the frequency count of employee last names, i.e.,
+-- how many employees share each last name.
+select last_name, COUNT(last_name)
+from employees
+group by last_name
+order by COUNT(last_name) DESC;
